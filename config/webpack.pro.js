085f6408ca00dbb41bb45c672dd4439622153a6b
sunny-wind-i <1229049792@qq.com>
2025-09-27 17:38:09 +0800

    feat: 压缩css

--- config/webpack.pro.js
+++ config/webpack.pro.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.base.js');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
   mode: 'production',
@@ -25,5 +26,11 @@ module.exports = merge(common, {
       filename: 'static/css/[name].[contenthash].css',
       chunkFilename: 'static/css/[id].[contenthash].css'
     })
-  ]
+  ],
+  optimization: {
+    minimizer: [
+      `...`,
+      new CssMinimizerPlugin()
+    ]
+  }
 });
